802822b58b115abb316cf1c59926383aef17de88
Paulo Campos <paulorcv@gmail.com>
2021-07-12 20:20:17 -0300

    Código inicial Consumidor e Produtor

---
+++ src/br/com/caelum/jms/TesteConsumidor.java
@@ -0,0 +1,55 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidor {
+	
+	public static void main (String args []) throws NamingException, JMSException {
+		
+		InitialContext context;
+	
+			context = new InitialContext();
+			ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+			Connection connection = factory.createConnection();
+			connection.start();
+
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			Destination fila = (Destination) context.lookup("financeiro");
+			MessageConsumer consumer = session.createConsumer(fila);
+			
+			
+			consumer.setMessageListener(new MessageListener() {
+				
+				@Override
+				public void onMessage(Message message) {
+					TextMessage textMessage = (TextMessage) message;
+					try {
+						System.out.println("---> Recebendo mensagem: " + textMessage.getText());
+					} catch (JMSException e) {
+						System.err.println("Erro:  " + e.getMessage());						
+					}
+				}
+			});
+			
+			
+			new Scanner(System.in).nextLine();
+
+			session.close();
+			connection.close();
+			context.close();
+		
+	}
+}
