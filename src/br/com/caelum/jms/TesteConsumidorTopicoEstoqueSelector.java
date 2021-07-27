5ccb05daaf0b719a92f606d1784984e338ed0dd9
Paulo Campos <paulorcv@gmail.com>
2021-07-27 07:30:50 -0300

    Modelo topico com seletores de mensagens no consumidor

--- src/br/com/caelum/jms/TesteConsumidorTopicoEstoqueSelector.java
+++ src/br/com/caelum/jms/TesteConsumidorTopicoEstoqueSelector.java
@@ -6,13 +6,15 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageProducer;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.jms.Topic;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-public class TesteProdutorTopico {
+public class TesteConsumidorTopicoEstoqueSelector {
 	
 	public static void main (String args []) throws NamingException, JMSException {
 		
@@ -21,20 +23,27 @@ public class TesteProdutorTopico {
 			context = new InitialContext();
 			ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
 			Connection connection = factory.createConnection();
+			connection.setClientID("estoque");
 			connection.start();
 
 			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			
 			Topic topico = (Topic) context.lookup("loja");
-			MessageProducer producer = session.createProducer(topico);
-		
-			for(int i=0; i<=100; i++) {
-				Message message = session.createTextMessage("(" + i + ") " + "Teste de envio de mensagem (ebook) ");
-				message.setBooleanProperty("ebook", false);
-				producer.send(message);
-			}
+			MessageConsumer consumer = session.createDurableSubscriber(topico, "assinatura-selector", "ebook is null OR ebook=false", false);
 			
 			
+			consumer.setMessageListener(new MessageListener() {
+				
+				@Override
+				public void onMessage(Message message) {
+					TextMessage textMessage = (TextMessage) message;
+					try {
+						System.out.println("---> Recebendo mensagem: " + textMessage.getText());
+					} catch (JMSException e) {
+						System.err.println("Erro:  " + e.getMessage());						
+					}
+				}
+			});
 			
 			
 			new Scanner(System.in).nextLine();
